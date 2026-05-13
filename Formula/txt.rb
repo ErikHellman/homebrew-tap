662ddb269b3683e2b52a5ac9f6a383163d673c67
Erik Hellman <erik.hellman@hellsoft.se>
2026-05-13 20:10:05 +0200

    Update txt to 0.6.0

--- Formula/txt.rb
+++ Formula/txt.rb
@@ -1,30 +1,30 @@
 class Txt < Formula
   desc "Terminal text editor"
   homepage "https://github.com/ErikHellman/txt"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/ErikHellman/txt/releases/download/v0.5.0/txt-v0.5.0-aarch64-apple-darwin.tar.gz"
-      sha256 "1ccbad56031054a9ca8ec0147955b94287b6104598a32f3b39562b7f504ecfe9"
+      url "https://github.com/ErikHellman/txt/releases/download/v0.6.0/txt-v0.6.0-aarch64-apple-darwin.tar.gz"
+      sha256 "aa10e0099362d314922b238ba76c584b6411e629540c95dfeb3b6766cfc07e28"
     end
 
     on_intel do
-      url "https://github.com/ErikHellman/txt/releases/download/v0.5.0/txt-v0.5.0-x86_64-apple-darwin.tar.gz"
-      sha256 "ce3f92504e91752864e582b2d827ea9aab2eca21cf46416cc559fcab8cd0997a"
+      url "https://github.com/ErikHellman/txt/releases/download/v0.6.0/txt-v0.6.0-x86_64-apple-darwin.tar.gz"
+      sha256 "302c3c5fe6ee0c1dcb4ccc294acb1e3d0d66941aa994d68ca5bb3e3d40915fb1"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/ErikHellman/txt/releases/download/v0.5.0/txt-v0.5.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "94d3cb1069303f9689313a1cd543bb4257b31e8544268fcaaf02cdd7641bd46c"
+      url "https://github.com/ErikHellman/txt/releases/download/v0.6.0/txt-v0.6.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "98f60e40867e1e0bb133b93d5d437edd5edfb3c05a10d16e478841cccb58d19e"
     end
 
     on_intel do
-      url "https://github.com/ErikHellman/txt/releases/download/v0.5.0/txt-v0.5.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "c5ecf31711433aa3e6a0f0845c1da5fafc1791d6e229e538edcd7ed4058aacd2"
+      url "https://github.com/ErikHellman/txt/releases/download/v0.6.0/txt-v0.6.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "09799f72b05dc669c7d1fccbe5b57b2238884a5555e47a64fa1609c3f966d1cc"
     end
   end
 
